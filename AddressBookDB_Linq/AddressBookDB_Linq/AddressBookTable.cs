a85e7ddcb66f46661000c9c020249bc9422535b5
Sharath <Sharus9587@gmail.com>
2021-06-14 07:45:38 +0530

    Added : Creating Data table with the attributes

--- AddressBookDB_Linq/AddressBookDB_Linq/AddressBookTable.cs
+++ AddressBookDB_Linq/AddressBookDB_Linq/AddressBookTable.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace AddressBookDB_Linq
 {
-    class AddressBookTable
+    class Linq_DataTable
     {
+        public void CreateDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(int));
+            table.Columns.Add("PhoneNumber", typeof(int));
+            table.Columns.Add("Email", typeof(string));
+
+        }
+
     }
 }
