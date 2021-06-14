c2225e5cb6a3edfeba889f76b97d62380cb36f8b
Sharath <Sharus9587@gmail.com>
2021-06-14 07:29:41 +0530

    Added : Creating Database

---
+++ AddressBookDB_Linq/AddressBookDB_Linq/AddressBookTable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookDB_Linq
+{
+    class AddressBookTable
+    {
+    }
+}
---
+++ AddressBookDB_Linq/AddressBookDB_Linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookDB_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
